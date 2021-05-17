a715ac476ea9354cacfc085321dadb4895a3b24b
Juan D. Jara <juanorigami@gmail.com>
2021-05-17 02:20:40 +0200

    refactor sql console to mutation pattern

--- src/views/SQLConsole.jsx
+++ src/views/SQLConsole.jsx
@@ -1,32 +1,12 @@
-import React, { useMemo, useState } from 'react'
+import React, { useState } from 'react'
 import CodeEditor from '@/components/common/CodeEditor'
 import Button from '@/components/common/Button'
-import useSQL from '@/lib/useSQL'
-import DataTable from 'react-data-table-component'
+import useSQLMutation from '@/lib/useSQLMutation'
+import Table from '@/components/Table'
 
 export default function SQLConsole() {
   const [query, setQuery] = useState('')
-  const [newQuery, setNewQuery] = useState('')
-  const { data, isLoading } = useSQL(query)
-
-  const columns = useMemo(() => {
-    if (!data[0]) {
-      return []
-    }
-
-    const keys = Object.keys(data[0])
-    return keys.map((key) => ({
-      name: key,
-      selector: key,
-      style: {
-        maxWidth: '200px'
-      }
-    }))
-  }, [data])
-
-  function runQuery() {
-    setQuery(newQuery)
-  }
+  const mutation = useSQLMutation()
 
   return (
     <div className="p-6 max-w-7xl">
@@ -35,37 +15,22 @@ export default function SQLConsole() {
         className="overflow-auto border-2 border-gray-300 rounded-lg"
       >
         <CodeEditor
-          value={newQuery}
-          onChange={setNewQuery}
+          value={query}
+          onChange={setQuery}
           style={{ minHeight: 'inherit' }}
         />
       </div>
-      <Button className="mt-4" onClick={runQuery} color="blue">
+      <Button
+        disabled={mutation.isLoading}
+        onClick={() => mutation.mutate(query)}
+        className="mt-4"
+        color="blue"
+      >
         Run Query
       </Button>
-      <div className="mt-8 border-gray-200">
-        {query && (
-          <DataTable
-            keyField="cartodb_id"
-            noHeader
-            responsive
-            pagination
-            loading={isLoading}
-            columns={columns}
-            data={data}
-            customStyles={{
-              pagination: {
-                style: {
-                  '& select': {
-                    minWidth: '36px'
-                  },
-                  '& select + svg': {
-                    display: 'none'
-                  }
-                }
-              }
-            }}
-          />
+      <div className="mt-8">
+        {mutation.isSuccess && (
+          <Table data={mutation.data} isLoading={mutation.isLoading} />
         )}
       </div>
     </div>
