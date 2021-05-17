a53d7b349c00955243d134e21f8c23535c4a121f
Juan D. Jara <juanorigami@gmail.com>
2021-05-17 03:13:46 +0200

    simplify aside

--- src/components/aside/Aside.jsx
+++ src/components/aside/Aside.jsx
@@ -1,5 +1,6 @@
 import { Link, useLocation } from '@reach/router'
 import React from 'react'
+import { buttonFocusStyle } from '@/components/common/Button'
 import AccountConfig from './AccountConfig'
 import FunctionList from './FunctionList'
 import SequenceList from './SequenceList'
@@ -13,9 +14,12 @@ export default function Aside() {
     <aside className="w-96 border-r border-gray-200 shadow-lg">
       <AccountConfig />
       <nav className="space-y-4 p-3">
-        <p className={`rounded-lg font-medium text-left text-blue-900 ${linkColor} focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-75`}>
-          <Link to="/console" className="block p-2 pr-3">SQL Console</Link>
-        </p>
+        <Link
+          to="/console"
+          className={`block p-2 pr-3 rounded-lg font-medium text-left text-blue-900 ${linkColor} ${buttonFocusStyle}`}
+        >
+          SQL Console
+        </Link>
         <TableList />
         <FunctionList />
         <SequenceList />
